0f907e03f257b71dff2eb5229551b8cf4e65b004
Omar Souid <omarsouid21@gmail.com>
2025-11-30 20:07:11 +0100

    feat: setupping structure

---
+++ shadcn-ui-sidebar-master/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+
+import "./globals.css";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+export const metadata: Metadata = {
+  title: "Smat City",
+  description: "Smart City BDD",
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={GeistSans.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <AdminPanelLayout>{children}</AdminPanelLayout>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
